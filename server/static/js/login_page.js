3ca3507d1514baba7466fdd06a7be8e96eae7754
dolev313 <dolevkahlon@gmail.com>
2025-08-28 10:06:05 +0300

    register page and js for login and register

---
+++ server/static/js/login_page.js
@@ -0,0 +1,11 @@
+function loginOnClick(){
+    let inputUsername = document.getElementById('username_id').value;
+    let inputPassword = document.getElementById('password_id').value;
+
+    alert(inputPassword);
+    alert(inputUsername);
+}
+
+function gotoRegister(){
+    window.location.href = "register.html";
+}
---
+++ server/static/js/register_page.js
@@ -0,0 +1,10 @@
+function registerOnClick(){
+    let inputUsername = document.getElementById('username_id').value;
+    let inputPassword = document.getElementById('password_id').value;
+    let inputPhone = document.getElementById('phone_id').value;
+
+}
+
+function gotoLogin(){
+    window.location.href = "Login_page.html";
+}
