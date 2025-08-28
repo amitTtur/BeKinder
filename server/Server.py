aad5e20b0a4bdf2cc7aaa5fcd26875aa11d2f339
skepp <skeppy032@gmail.com>
2025-08-28 09:11:35 +0300

    aass

--- server/Server.py
+++ server/Server.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify, send_file, render_template, make_response
 from flask_cors import CORS  # Import CORS
+from community_place import CommunityPlace
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 name_list = []
+IMAGES_FOLDER = os.path.join(os.getcwd(), "static/images")
 
 
 @app.route('/', methods=['GET'])
@@ -27,6 +30,28 @@ def loginCalc():
     response = make_response(name_list)
     response.set_cookie('username', username)
     return response
+
+
+@app.route('/community_places', methods=['GET'])
+def get_community_places():
+    places = []
+    for idx, filename in enumerate(os.listdir(IMAGES_FOLDER)):
+        description = f"This is a description for {filename}"
+        picture_path = f"/static/images/{filename}"
+        place = CommunityPlace(
+            name=filename.split('.')[0],
+            description=description,
+            picture_path=picture_path,
+            id=idx
+        )
+        places.append({
+            "id": place.id,
+            "name": place.name,
+            "description": place.description,
+            "picture_path": place.picture_path
+        })
+
+    return jsonify(places)
 
 
 if __name__ == '__main__':
