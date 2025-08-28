26b79517360453e54695a3ad3b4f602a582c47c7
skepp <skeppy032@gmail.com>
2025-08-28 11:48:40 +0300

    aass

--- server/Server.py
+++ server/Server.py
@@ -3,11 +3,13 @@ from flask_cors import CORS  # Import CORS
 from community_place import CommunityPlace
 import os
 
+from db_users import *
+
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 name_list = []
 IMAGES_FOLDER = os.path.join(os.getcwd(), "static/images")
-
+i = 0
 
 @app.route('/', methods=['GET'])
 def getFirstPage():
@@ -24,15 +26,29 @@ def getIndexPage():
     return render_template('/index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
+def loginCalc():
+    # Get the image URL from the query parameters
+
+    username = request.form.get('username')
+    print('Received download: ', username)
+    password = request.form.get('password')
+    print('Received download: ', password)
+
+
 def login():
     username = request.args.get('username')
     print(username)
     password = request.args.get('password')
     print(password)
     name_list.append(username)
-    #
     response = make_response(name_list)
     response.set_cookie('username', username)
+    result = login_user(username, password)
+
+    if result is None:
+        response.status_code = 404
+    else:
+        response.status_code = 200
     response.status_code = 200
     return response
 
@@ -56,6 +72,7 @@ def getRegister():
     response.status_code = 200
     return response
 
+
 @app.route('/Login_page', methods=['GET'])
 def getLoginPageAll():
     return render_template('/Login_page.html')
@@ -66,6 +83,7 @@ def getRegisterPageAll():
 
 @app.route('/community_places', methods=['GET'])
 def get_community_places():
+    global i
     places = []
     for idx, filename in enumerate(os.listdir(IMAGES_FOLDER)):
         description = f"This is a description for {filename}"
@@ -82,8 +100,13 @@ def get_community_places():
             "description": place.description,
             "picture_path": place.picture_path
         })
-
-    return jsonify(places)
+    response = make_response()
+    response.status_code = 200
+    response.data = jsonify(list[i])
+    i += 1
+    if i > len(places):
+        i = 0
+    return response
 
 
 if __name__ == '__main__':
